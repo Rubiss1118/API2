8a0da6ce100c645af3c3137a7897b3b195eda923
Rubiss1118 <emo63703@gmail.com>
2025-07-15 00:40:56 -0600

    Fix: Recompilado y copiado 404.html para Pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/APIS',
+  basePath: '/API2',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,38 +2,38 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: false,
-  baseHref: '/APIS/',
+  baseHref: '/API2/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "redirectTo": "/APIS/login",
-    "route": "/APIS"
+    "redirectTo": "/API2/login",
+    "route": "/API2"
   },
   {
     "renderMode": 2,
-    "route": "/APIS/login"
+    "route": "/API2/login"
   },
   {
     "renderMode": 2,
-    "route": "/APIS/registro"
+    "route": "/API2/registro"
   },
   {
     "renderMode": 2,
-    "route": "/APIS/bienvenida"
+    "route": "/API2/bienvenida"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/APIS/login",
-    "route": "/APIS/**"
+    "redirectTo": "/API2/login",
+    "route": "/API2/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 16600, hash: 'f4af26ded78b9ebfabf1771358078983b2af7980259383507ff4de29b1722b1d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17140, hash: 'a4d64c9e5b7807bb0e20dcdcb65ded7feb594372a381d9f10c6039116fd4bd55', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'bienvenida/index.html': {size: 20846, hash: '0686accb437aa9779e09b892f74abc239041c7b2e5bfa1b94253314b077a2c2a', text: () => import('./assets-chunks/bienvenida_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 20846, hash: '0686accb437aa9779e09b892f74abc239041c7b2e5bfa1b94253314b077a2c2a', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'registro/index.html': {size: 21922, hash: '31753313ccf11080a97e4003e6cab6d43a6c02724fedf78909317442145e7389', text: () => import('./assets-chunks/registro_index_html.mjs').then(m => m.default)}
+    'index.csr.html': {size: 16600, hash: '808c6da22153f52c81f34b6db8120be20596aa397ca01420797c27c94e7828aa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17140, hash: '56378f6128eaa4b2aae02d8880ef3003875de2af0f7694c5201d90d4a84f78c8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'bienvenida/index.html': {size: 20846, hash: '955425271663fadb81bdb4fb1aa4e9078f87e25a461ffed91432a864d749cced', text: () => import('./assets-chunks/bienvenida_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 20846, hash: '955425271663fadb81bdb4fb1aa4e9078f87e25a461ffed91432a864d749cced', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'registro/index.html': {size: 21922, hash: '1e5f72b390f8e42fa7148d7b85dcdd565304e723130825de3b64214aadfc2f89', text: () => import('./assets-chunks/registro_index_html.mjs').then(m => m.default)}
   },
 };
